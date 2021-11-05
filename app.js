c122de3102143dbd23c342eecc3bf1d963633a95
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-11-05 11:31:34 -0700

    base html structure, incl sections for 4 math operations add, subtract, multiply, divide.

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// HTML SETUP
+// Build a calculator that will +-*/ 2 numbers
+// Create an input for 2 numbers and an input which submits those 2 numbers for calculation, for each math operator.
+
+
 // import functions and grab DOM elements
 
 // initialize state
