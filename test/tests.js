c3c9df6e032e692d55933da7a1bdce9f6d2d319b
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-11-05 14:50:31 -0700

    passing and failing test for each math operator function

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,7 @@
 import { addition } from '../math-utils.js';
+import { subtraction } from '../math-utils.js';
+import { division } from '../math-utils.js';
+import { multiplication } from '../math-utils.js';
 
 const test = QUnit.test;
 
@@ -10,9 +13,58 @@ test('time to test a function', (expect) => {
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = addition(1, 2);
-    const actual = addition(1, 3);
+    // const actual = addition(1, 3);
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+
+test('time to test a function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 8;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subtraction(10, 2);
+    // const actual = subtraction(8, 2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('time to test a function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 10;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multiplication(2, 5);
+    // const actual = multiplication(3, 6);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('time to test a function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 2;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = division(8, 4);
+    // const actual = division(10, 2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+
